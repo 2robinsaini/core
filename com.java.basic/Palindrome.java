4feb481d308726ebfed0d00434ec3f77b68bd281
Robin <2robinsaini@gmail.com>
2024-11-01 19:30:48 +0530

    --Added Palindrome

---
+++ com.java.basic/Palindrome.java
@@ -0,0 +1,25 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        Palindrome palindrome = new Palindrome();
+        int number = 12321;
+        String input = "madam";
+        boolean isNumberPalindrome = palindrome.checkNumberPalindrome(number);
+        boolean isStringPalindrome = palindrome.checkStringPalindrome(input);
+        System.out.println("Number " + number +" is palindrome: " + isNumberPalindrome);
+        System.out.println("String " + input +" is palindrome: " + isStringPalindrome);
+    }
+
+    private boolean checkNumberPalindrome(int numberString) {
+         return checkStringPalindrome(Integer.valueOf(numberString).toString());
+    }
+
+    private boolean checkStringPalindrome(String input){
+        char[] chars = input.toCharArray();
+        for(int i=0;i<chars.length/2;i++){
+            if(chars[i]!=chars[chars.length-1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
